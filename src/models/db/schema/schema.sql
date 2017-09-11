34ad884e0b0dd3e0ed9a6502577839724ccc2050
Breyana Scales <breyanaj@gmail.com>
2017-09-11 11:14:58 -0700

    Add users to schema

--- src/models/db/schema/schema.sql
+++ src/models/db/schema/schema.sql
@@ -1,7 +1,15 @@
 DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE contacts (
   id serial,
   first_name varchar(255) NOT NULL,
   last_name varchar(255) NOT NULL
+);
+
+CREATE TABLE users (
+  user_id serial,
+  username varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  admin boolean NOT NULL 
 );
